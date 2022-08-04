8559d469524da3a083278b50d945f3a5e9ce06bb
And4Web <anands2807@gmail.com>
2022-08-04 10:41:26 +0530

    created socket.io server and started listening

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-const io = require('socket.io');
+const {Server} = require('socket.io');
 
 const app = express();
 
+app.use(cors());
+
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  },
+});
+
+server.listen(5000, ()=>{
+  console.log("Listening at port: 5000")
+})
